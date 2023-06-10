f4c39159936d0407e25ab0cc530c0326297d2775
rjindael <me@rjindael.xyz>
2023-06-10 01:58:42 -0700

    Finalize help function styling, and add screenshot to README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # rcctalk
 RCCService communication utility
 
+# Screenshot
+![](https://github.com/kiseki-lol/rcctalk/raw/trunk/screenshot.png)
+
 # Usage
 rcctalk is a console application that can be installed with the command `npm i -g`. For more instructions, please run `npx rcctalk --help`.
 
--- app.js
+++ app.js
@@ -2,18 +2,28 @@ import chalk from "chalk"
 
 import soap from "./soap.js"
 
+// TODO: These chalk functions are disgusting but it's a good mockup for what it'll look like in the finished product.
+// Refactor these later.
 function displayOperations() {
     console.log(chalk.white("Available operations:"))
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("OpenJob")}${chalk.grey("(")}${chalk.redBright("string")} JobId, ${chalk.magentaBright("int?")} ExpirationInSeconds = ${chalk.cyanBright("300")}, ${chalk.magentaBright("int?")} Category = ${chalk.cyanBright("0")}, ${chalk.magentaBright("int?")} Cores = ${chalk.cyanBright("1")}, ${chalk.redBright("string")} Script, ${chalk.grey(`[... Arguments]`)}${chalk.grey(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("BatchJob")}${chalk.grey("(")}${chalk.redBright("string")} JobId, ${chalk.magentaBright("int?")} ExpirationInSeconds = ${chalk.cyanBright("300")}, ${chalk.magentaBright("int?")} Category = ${chalk.cyanBright("0")}, ${chalk.magentaBright("int?")} Cores = ${chalk.cyanBright("1")}, ${chalk.redBright("string")} Script, ${chalk.grey("[... Arguments]")}${chalk.grey(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseJob")}${chalk.grey("(")}${chalk.redBright("string")} JobId${chalk.grey(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseAllJobs")}${chalk.grey("()")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseAllExpiredBrightJobs")}${chalk.grey("()")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("RenewLease")}${chalk.grey("(")}${chalk.redBright("string")} JobId, int ExpirationInSeconds${chalk.grey(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("GetExpiration")}${chalk.grey("(")}${chalk.redBright("string")} JobId${chalk.grey(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.greenBright("ExecuteResult")} ${chalk.yellowBright("ExecuteScript")}${chalk.grey("(")}${chalk.redBright("string")} JobId, ${chalk.redBright("string")} Script, ${chalk.grey("[... Arguments]")}${chalk.grey(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.greenBright("GetStatusResult")} ${chalk.yellowBright("GetStatus")}${chalk.grey("()")}`)
-    console.log(`${chalk.grey("-")} ${chalk.greenBright("DiagResult")} ${chalk.yellowBright("Diag")}${chalk.grey("(")}${chalk.redBright("int")} Type, ${chalk.redBright("string")} JobId${chalk.grey(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("OpenJob")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.magentaBright("int?")} ExpirationInSeconds = ${chalk.cyanBright("300")}, ${chalk.magentaBright("int?")} Category = ${chalk.cyanBright("0")}, ${chalk.magentaBright("int?")} Cores = ${chalk.cyanBright("1")}, ${chalk.redBright("string")} Script, ${chalk.grey(`[... Arguments]`)}${chalk.whiteBright(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("BatchJob")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.magentaBright("int?")} ExpirationInSeconds = ${chalk.cyanBright("300")}, ${chalk.magentaBright("int?")} Category = ${chalk.cyanBright("0")}, ${chalk.magentaBright("int?")} Cores = ${chalk.cyanBright("1")}, ${chalk.redBright("string")} Script, ${chalk.grey("[... Arguments]")}${chalk.whiteBright(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseJob")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId${chalk.whiteBright(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseAllJobs")}${chalk.whiteBright("()")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseAllExpiredJobs")}${chalk.whiteBright("()")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("RenewLease")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.redBright("int")} ExpirationInSeconds${chalk.whiteBright(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("GetExpiration")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId${chalk.whiteBright(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.greenBright("ExecuteResult")} ${chalk.yellowBright("ExecuteScript")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.redBright("string")} Script, ${chalk.grey("[... Arguments]")}${chalk.whiteBright(")")}`)
+    console.log(`${chalk.grey("-")} ${chalk.greenBright("GetStatusResult")} ${chalk.yellowBright("GetStatus")}${chalk.whiteBright("()")}`)
+    console.log(`${chalk.grey("-")} ${chalk.greenBright("DiagResult")} ${chalk.yellowBright("Diag")}${chalk.whiteBright("(")}${chalk.redBright("int")} Type, ${chalk.redBright("string")} JobId${chalk.whiteBright(")")}`)
+}
+
+function displayCommands() {
+    console.log(chalk.white("Available commands:"))
+    console.log(`${chalk.grey("-")} ${chalk.whiteBright("connect <ip>")} - connects to a different RCCService instance`)
+    console.log(`${chalk.grey("-")} ${chalk.whiteBright("operations")} - displays all available operations`)
+    console.log(`${chalk.grey("-")} ${chalk.whiteBright("version")} - output the version number`)
+    console.log(`${chalk.grey("-")} ${chalk.whiteBright("exit")} - closes rcctalk`)
 }
 
 function displayLogo() {
