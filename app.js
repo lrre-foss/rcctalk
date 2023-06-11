3b9caee1f620384abd9de84d150cff15a89f4534
rjindael <me@rjindael.xyz>
2023-06-11 16:32:23 -0700

    Operations + bug fix

--- app.js
+++ app.js
@@ -19,40 +19,8 @@ function displayLogo() {
     console.log(`version ${version}\n`)
 }
 
-// TODO: These chalk functions are disgusting but it's a good mockup for what it'll look like in the finished product.
-// Refactor these later.
-function displayOperations() {
-    console.log(chalk.white("Available operations:"))
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("OpenJob")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.magentaBright("int?")} ExpirationInSeconds = ${chalk.cyanBright("300")}, ${chalk.magentaBright("int?")} Category = ${chalk.cyanBright("0")}, ${chalk.magentaBright("int?")} Cores = ${chalk.cyanBright("1")}, ${chalk.redBright("string")} Script, ${chalk.grey(`[... Arguments]`)}${chalk.whiteBright(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("BatchJob")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.magentaBright("int?")} ExpirationInSeconds = ${chalk.cyanBright("300")}, ${chalk.magentaBright("int?")} Category = ${chalk.cyanBright("0")}, ${chalk.magentaBright("int?")} Cores = ${chalk.cyanBright("1")}, ${chalk.redBright("string")} Script, ${chalk.grey("[... Arguments]")}${chalk.whiteBright(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseJob")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId${chalk.whiteBright(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseAllJobs")}${chalk.whiteBright("()")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("CloseAllExpiredJobs")}${chalk.whiteBright("()")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("RenewLease")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.redBright("int")} ExpirationInSeconds${chalk.whiteBright(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.blueBright("void")} ${chalk.yellowBright("GetExpiration")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId${chalk.whiteBright(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.greenBright("ExecuteResult")} ${chalk.yellowBright("ExecuteScript")}${chalk.whiteBright("(")}${chalk.redBright("string")} JobId, ${chalk.redBright("string")} Script, ${chalk.grey("[... Arguments]")}${chalk.whiteBright(")")}`)
-    console.log(`${chalk.grey("-")} ${chalk.greenBright("GetStatusResult")} ${chalk.yellowBright("GetStatus")}${chalk.whiteBright("()")}`)
-    console.log(`${chalk.grey("-")} ${chalk.greenBright("DiagResult")} ${chalk.yellowBright("Diag")}${chalk.whiteBright("(")}${chalk.redBright("int")} Type, ${chalk.redBright("string")} JobId${chalk.whiteBright(")")}`)
-    console.log()
-}
-
-function displayCommands() {
-    console.log(chalk.white("Available commands:"))
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("connect <ip>")} - connects to a different RCCService instance`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("disconnect")} - disconnects from the current RCCService instance`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("version")} - output the version number of the connected RCCService instance`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("ping")} - pings the connected RCCService instance`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("help")} - displays this help message`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("operations")} - displays all available operations`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("commands")} - displays all available commands`)
-    console.log(`${chalk.grey("-")} ${chalk.whiteBright("exit")} - closes rcctalk`)
-    console.log()
-}
-
 function start(options) {
     displayLogo()
-    displayOperations()
-    displayCommands()
 }
 
 export default { version, start }
--- shell.js
+++ shell.js
@@ -2,6 +2,18 @@ import chalk from "chalk"
 
 import connection from "./connection.js"
 
+// Quickly access chalk functions @_@
+const c = {
+    r: chalk.redBright,
+    g: chalk.greenBright,
+    y: chalk.yellowBright,
+    m: chalk.magentaBright,
+    c: chalk.cyanBright,
+    w: chalk.whiteBright,
+    b: chalk.blueBright,
+    gr: chalk.grey
+}
+
 const commands = {
     "connect": {
         description: "connect to a RCCService instance",
@@ -49,21 +61,84 @@ const commands = {
     "operations": {
         description: "displays all available operations",
         handler: () => {
+            console.log(c.w("Available operations:"))
 
+            for (let name in operations) {
+                let operation = operations[name]
+
+                process.stdout.write(`${c.gr("-")} `)
+
+                // Operation type
+                if (typeof operation.returns !== "object") {
+                    process.stdout.write(`${c.b(operation.returns)}`)
+                } else {
+                    let type = operation.returns.type
+                    
+                    if (type.includes("[]")) {
+                        process.stdout.write(`${c.g(type.replace("[]", ""))}${c.w("[]")}`)
+                    } else {
+                        process.stdout.write(`${c.g(type)}`)
+                    }
+                }
+
+                // Operation name
+                process.stdout.write(` ${c.y(name)}(`)
+
+                // Operation parameters
+                if (!operation.hasOwnProperty("parameters")) {
+                    console.log(")")
+                } else {
+                    let i = 0
+                    for (let name in operation.parameters) {
+                        let parameter = operation.parameters[name]
+
+                        // Parameter type & name
+                        if (parameter.required) {
+                            process.stdout.write(`${c.r(parameter.type)} ${c.w(name)}`)
+                        } else {
+                            process.stdout.write(`${c.r(parameter.type)}${c.m("?")} ${c.w(name)}`)
+
+                            if (parameter.hasOwnProperty("default")) {
+                                process.stdout.write(` = ${c.c(typeof parameter.default === "object" ? "[]" : parameter.default)}`)
+                            }
+                        }
+
+                        if (i == Object.keys(operation.parameters).length - 1) {
+                            console.log(")")
+                        } else {
+                            process.stdout.write(", ")
+                        }
+                        i++
+                    }
+                }
+            }
+
+            console.log()
         }
     },
     "commands": {
         description: "displays all available commands",
         handler: () => {
-            console.log(chalk.whiteBright("Available commands:"))
-            for (let command in commands) {
-                console.log(`${chalk.grey("-")} ${chalk.whiteBright(command)}`)
-                for (let parameter of commands[command].parameters) {
-                    console.log(` ${chalk.whiteBright(parameter)}`)
+            console.log(c.w("Available commands:"))
+
+            for (let name in commands) {
+                let command = commands[name]
+
+                // Command name
+                process.stdout.write(`${c.gr("-")} ${c.w(name)}`)
+
+                // Parameters
+                if (command.hasOwnProperty("parameters")) {
+                    for (let parameter of command.parameters) {
+                        process.stdout.write(` <${c.w(parameter)}>`)
+                    }
                 }
 
-                console.log(` - ${commands[command].description}`)
+                // Command description
+                console.log(` - ${command.description}`)
             }
+
+            console.log()
         }
     },
     "exit": {
@@ -74,4 +149,168 @@ const commands = {
     }
 }
 
-const operations = []
+const operations = {
+    "HelloWorld": {
+        returns: "string",
+    },
+    "GetVersion": {
+        returns: "string",
+    },
+    "GetStatus": {
+        returns: {
+            userdata: true,
+            type: "Status",
+        }
+    },
+    "OpenJob": {
+        returns: {
+            userdata: true,
+            type: "LuaValue[]",
+        },
+        parameters: {
+            "jobID": {
+                type: "string",
+                required: true
+            },
+            "expirationInSeconds": {
+                type: "double",
+                required: false,
+                default: 300.0
+            },
+            "category": {
+                type: "int",
+                required: false,
+                default: 0
+            },
+            "cores": {
+                type: "int",
+                required: false,
+                default: 1
+            },
+            "script": {
+                type: "string",
+                required: true
+            },
+            "arguments": {
+                type: "array",
+                required: false,
+                default: []
+            }
+        },
+    },
+    "RenewLease": {
+        returns: "double",
+        parameters: {
+            "jobID": {
+                type: "string",
+                required: true
+            },
+            "expirationInSeconds": {
+                type: "double",
+                required: true
+            }
+        }
+    },
+    "Execute": {
+        returns: {
+            userdata: true,
+            type: "LuaValue[]",
+        },
+        parameters: {
+            "jobID": {
+                type: "string",
+                required: true
+            },
+            "script": {
+                type: "string",
+                required: true
+            },
+            "arguments": {
+                type: "array",
+                required: false,
+                default: []
+            }
+        }
+    },
+    "CloseJob": {
+        returns: "void",
+        parameters: {
+            "jobID": {
+                type: "string",
+                required: true
+            },
+        }
+    },
+    "BatchJob": {
+        returns: {
+            userdata: true,
+            type: "LuaValue[]",
+        },
+        parameters: {
+            "jobID": {
+                type: "string",
+                required: true
+            },
+            "expirationInSeconds": {
+                type: "double",
+                required: false,
+                default: 300.0
+            },
+            "category": {
+                type: "int",
+                required: false,
+                default: 0
+            },
+            "cores": {
+                type: "int",
+                required: false,
+                default: 1
+            },
+            "script": {
+                type: "string",
+                required: true
+            },
+            "arguments": {
+                type: "array",
+                required: false,
+                default: []
+            }
+        },
+    },
+    "GetExpiration": {
+        returns: "double",
+        parameters: {
+            "jobID": {
+                type: "string",
+                required: true
+            },
+        }
+    },
+    "GetAllJobs": {
+        returns: {
+            userdata: true,
+            type: "Job[]"
+        }
+    },
+    "CloseExpiredJobs": {
+        returns: "int"
+    },
+    "Diag": {
+        returns: {
+            userdata: true,
+            type: "LuaValue[]"
+        },
+        parameters: {
+            "type": {
+                type: "int",
+                required: true
+            },
+            "jobID": {
+                type: "string",
+                required: false
+            }
+        }
+    }
+}
+
+export default {}
