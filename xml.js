c9795338b0add53851624f5a2b5fe0513d852fee
rjindael <me@rjindael.xyz>
2023-06-11 20:31:01 -0700

    Lua XML parsing

--- xml.js
+++ xml.js
@@ -1,6 +1,7 @@
-import xmlFormat from "xml-formatter"
 import { XMLParser } from "fast-xml-parser"
 import colorize from "json-colorizer"
+import xmlFormat from "xml-formatter"
+
 const template = 
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
     "<SOAP-ENV:envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:ns1=\"http://roblox.com/\" xmlns:ns2=\"http://roblox.com/RCCServiceSoap\" xmlns:ns3=\"http://roblox.com/RCCServiceSoap12\">" +
@@ -38,16 +39,6 @@ function getLuaType(value) {
     return type
 }
 
-function generateLuaArguments(data) {
-    let xml = ""
-
-    for (let argument of data) {
-        xml += generateLuaValueXml(argument)
-    }
-
-    return xml
-}
-
 function generateLuaValueXml(value) {
     let xml = ""
     xml += "<ns1:LuaValue>"
@@ -61,10 +52,20 @@ function generateLuaValueXml(value) {
         xml += "</ns1:table>"
     } else {
         xml += `<ns1:type>${getLuaType(value)}</ns1:type>`
-        xml += `<ns1:value>${value}</ns1:value>`
+        xml += `<ns1:value>${getLuaType(value) == "LUA_TNIL" ? "" : value}</ns1:value>`
     }
 
     xml += "</ns1:LuaValue>"
+    return xml
+}
+
+function generateLuaArguments(data) {
+    let xml = ""
+
+    for (let argument of data) {
+        xml += generateLuaValueXml(argument)
+    }
+
     return xml
 }
 
@@ -101,6 +102,42 @@ function generateEnvelope(operations) {
     return xmlFormat(template.replace("{{body}}", xml))
 }
 
+function getJsValueFromLuaXml(xml) {
+    switch (xml["ns1:type"]) {
+        case "LUA_TBOOLEAN":
+            return xml["ns1:value"] == "true"
+        case "LUA_TNUMBER":
+            return Number(xml["ns1:value"])
+        case "LUA_TSTRING":
+            return xml["ns1:value"]
+        case "LUA_TNIL":
+            return null
+        default:
+            return xml["ns1:value"]
+    }
+}
+
+function parseLuaValueXml(value) {
+    let parsed = []
+
+    for (let element of value) {
+        if (element.hasOwnProperty("ns1:value")) {
+            parsed.push(getJsValueFromLuaXml(element))
+        } else {
+            let values = Object.values(element["ns1:table"])
+            let table = []
+
+            for (let value of values) {
+                table.push(getJsValueFromLuaXml(value))
+            }
+
+            parsed.push(table)
+        }
+    }
+
+    return parsed
+}
+
 function parseEnvelope(envelope) {
     let response = {
         "success": false,
@@ -122,29 +159,34 @@ function parseEnvelope(envelope) {
         response.error = body["SOAP-ENV:Fault"]["faultstring"]
         return response
     }
-    
+
     body = Object.values(body)[0]
-
-    if (Object.values(body).length == 1) {
-        body = Object.values(body)[0]
-    }
-
-    if (typeof body === "object") {
-        let reconstructed = []
-
-        for (let element of body) {
-            let keys = Object.keys(element)
-            let values = Object.values(element)
-            let cleaned = {}
+    let type = Object.keys(body)[0].split(":")[1].replace("Result", "")
+    if (type == "OpenJob" || type == "Execute" || type == "BatchJob" || type == "Diag") {
+        // This is a LuaValue[]
+        body = parseLuaValueXml(Object.values(body)[0])
+    } else {
+        if (Object.values(body).length == 1) {
+            body = Object.values(body)[0]
+        }
     
-            for (let key of keys) {
-                cleaned[key.split(":")[1]] = values[keys.indexOf(key)]
+        if (typeof body === "object") {
+            let reconstructed = []
+    
+            for (let element of body) {
+                let keys = Object.keys(element)
+                let values = Object.values(element)
+                let cleaned = {}
+        
+                for (let key of keys) {
+                    cleaned[key.split(":")[1]] = values[keys.indexOf(key)]
+                }
+        
+                reconstructed.push(cleaned)
             }
     
-            reconstructed.push(cleaned)
+            body = reconstructed
         }
-
-        body = reconstructed
     }
 
     response.success = true
