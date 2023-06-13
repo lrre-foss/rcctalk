86418e4e37edd82ac1919879183dadb2c0d9d211
rjindael <me@rjindael.xyz>
2023-06-13 00:56:43 -0700

    Remove debugging stuff

--- xml.js
+++ xml.js
@@ -1,5 +1,5 @@
 import { XMLParser } from "fast-xml-parser"
-import colorize from "json-colorizer"
+
 const template = 
     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
     "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:ns1=\"http://roblox.com/\" xmlns:ns2=\"http://roblox.com/RCCServiceSoap\" xmlns:ns3=\"http://roblox.com/RCCServiceSoap12\">" +
@@ -213,7 +213,6 @@ function parseEnvelope(envelope) {
             }
         }
     } catch (e) {
-        throw e
         response.error = `Failed to parse response - ${e}`
         return response
     }
